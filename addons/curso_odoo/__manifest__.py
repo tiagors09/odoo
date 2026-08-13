c1b0d374a4679cfb6a4330f2e41ae4fe58750979
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2026-08-13 15:25:10 -0300

    initialize curso_odoo module with basic structure and configuration

---
+++ addons/curso_odoo/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Curso Odoo',
+    'version': '1.0',
+    'summary': 'Aulas de capacitação do curso de Odoo',
+    'description': """
+        Curso de Odoo
+    """,
+    'category': 'Accounting/Accounting',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'author': 'tiagors09',
+    'license': 'LGPL-3',
+}
